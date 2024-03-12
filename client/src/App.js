ae25ebd7c54293cefa9e997523d7b190e64e7f73
Shiva <shiva.t2success@gmail.com>
2024-03-12 18:41:52 +0530

    Client User Connected Script Updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import io from 'socket.io-client'
+const socket = io.connect("http://localhost:3001")
 function App() {
+    const sendMessage = () => {
+      // socket.emit()
+    }
   return (
     <div className="App">
-      
+      <input placeholder='Message...' />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
